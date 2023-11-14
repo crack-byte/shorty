8943715deaabc534a2aeed3f7d281f41658a47de
crack-byte <crackbyte@proton>
2023-11-14 16:52:57 +0530

    added a fix

--- src/app/api/shorten/route.ts
+++ src/app/api/shorten/route.ts
@@ -23,9 +23,11 @@ export async function GET(req: NextRequest): Promise<NextResponse<any>> {
     const params = await req.nextUrl.searchParams;
     const hash: any = params.get("hash");
     try {
-        const op = new Map<string, any>();
-        req.headers.forEach((value, key) => op.set(key, value))
-        FirebaseService.writeDataToIps({headers: JSON.stringify(op), url: req.url, timestamp: new Date()})
+        const o: any = {};
+        req.headers.forEach((value, key) => {
+            o[key] = value;
+        })
+        FirebaseService.writeDataToIps({headers: JSON.stringify(o), url: req.url, timestamp: new Date()})
         const doc = await FirebaseService.findMapping("shortUrl", hash);
         return NextResponse.json({value: doc.data().originalUrl}, {status: 200});
     } catch (error: any) {
